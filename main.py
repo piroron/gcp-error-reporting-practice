fa0f12efd94537b684271801030cd357e5bff87f
eiji.maeda <eiji.maeda@sansan.com>
2022-09-09 10:42:23 +0900

    print stack trace message

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from google.cloud.errorreporting_v1beta1.services.error_stats_service import ErrorStatsServiceClient
 from google.cloud.errorreporting_v1beta1.services.error_group_service import ErrorGroupServiceClient
 from google.cloud.errorreporting_v1beta1.types.error_group_service import GetGroupRequest
+from google.cloud.errorreporting_v1beta1.types.error_stats_service import QueryTimeRange
 from config import setting
 
 
@@ -9,16 +10,20 @@ def main():
     client = ErrorStatsServiceClient()
     # get stats
     stats = client.list_group_stats(
-        project_name=setting.project_name)
-    for stat in stats:
-        print(stat)
-        print(f'jsonName: {stat.group}')
-    else:
-        print('Errors not exist.')
+        project_name=setting.project_name,
+        time_range=QueryTimeRange(period=QueryTimeRange.Period.PERIOD_30_DAYS)
+    )
+    for item in stats:
+        # print error stack trace
+        print(item.representative.message)
+        # print(item.group)
+        
+        # for stats in item.error_group_stats:
+        #     print(stats)
 
     # get event
     # events = client.list_events(
-    #     project_name=setting.project_name, group_id='CIyZtaPD5OfQVg')
+    #     project_name=setting.project_name)
     # for p in events.pages:
     #     print(p.time_range_begin)
     #     print(p.error_events)
